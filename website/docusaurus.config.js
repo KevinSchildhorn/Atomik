3b17bc4af11e4a18848bc010bfcd646061cabce6
Kevin Schildhorn <kevins@touchlab.co>
2023-06-09 18:29:22 -0400

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Atomik/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kevinschildhorn', // Usually your GitHub org/user name.
-  projectName: 'atomik', // Usually your repo name.
+  organizationName: 'kevinschildhorn',
+  projectName: 'atomik',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
